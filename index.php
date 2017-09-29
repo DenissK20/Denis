6e421cf6f21faa41292405a65776d868c16dae96
DenissK20 <deniska.riga@gmail.com>
2017-09-29 15:34:13 +0300

    Update

--- index.php
+++ index.php
@@ -2,3 +2,6 @@
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Загрузка наших мозгов...');
 $app->initLayout('Centered');
+$button = $app->add('Button');
+$button->set('Add Invoice');
+$button->icon('plus');
