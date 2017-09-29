a75dd0a2f109481c3f84e892c6b4f9c0369e91e8
DenissK20 <deniska.riga@gmail.com>
2017-09-29 15:49:41 +0300

    Update

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Загрузка наших мозгов...');
 $app->initLayout('Centered');
 $button = $app->add('Button');
-$button->set('Registration');
+$button->set('Follower');
 $button->icon = 'plus';
-$button->link ('https://www.e-klase.lv');
+$button->link ('https://www.youtube.com');
