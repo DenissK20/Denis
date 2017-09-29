db1f4ab13482dd3b61f69cf97e696bd10936e0d2
DenissK20 <deniska.riga@gmail.com>
2017-09-29 15:44:48 +0300

    Update

--- index.php
+++ index.php
@@ -3,5 +3,6 @@ require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Загрузка наших мозгов...');
 $app->initLayout('Centered');
 $button = $app->add('Button');
-$button->set('Add Invoice');
+$button->set('Registration');
 $button->icon = 'plus';
+$button->link ('https://www.e-klase.lv');
