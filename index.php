402bd1ed2576df8cd4845fa3f814558f5b3cbed3
DenissK20 <deniska.riga@gmail.com>
2017-09-29 15:36:06 +0300

    Updaet

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ $app = new \atk4\ui\App('Загрузка наших мозгов...');
 $app->initLayout('Centered');
 $button = $app->add('Button');
 $button->set('Add Invoice');
-$button->icon('plus');
+$button->icon = 'plus';
