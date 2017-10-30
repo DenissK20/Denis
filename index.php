bfdf71ef42a2360a2cf17a216ede5886d4a70c16
DenissK20 <deniska.riga@gmail.com>
2017-10-30 17:32:12 +0200

    Update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Добро пожаловать на страницу ссылок!');
 $app->initLayout('Centered');
-$button = $app->add('Button');
+/*$button = $app->add('Button');
 $button->set('My chess site');
 $button->addClass('massive grey');
 $button->icon = 'delicious';
@@ -27,4 +27,7 @@ $button->set('My web site');
 $button->addClass('large purple');
 $button->icon = 'browser';
 $button->link ('http://mysecondwork.herokuapp.com');
-$button=$app->add(['Button','New','medium teal','iconRight'=>'right arrow',]);
+$button=$app->add(['Button','New','medium teal','iconRight'=>'right arrow',]);*/
+$label = $app->add(['Label', 'Book','medium red','icon'=>'book']);
+$label = $app->add(['Label', 'Book','medium red','image'=>'http://greetingbooks.com/wp-content/uploads/2015/08/3.png']);
+$text = $app->add(['Text', 'My book...']);
--- index_2.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = new \atk4\ui\App('Добро пожаловать на новый сайт!');
-$app->initLayout('Centered');
-$button=$app->add(['Button','Back','medium teal','iconLeft'=>'left arrow','http://mysecondwork.herokuapp.com']);
