2e37dc58bcc63acf1a52e55e0d94b51f49c84020
DenissK20 <deniska.riga@gmail.com>
2017-12-03 18:43:25 +0200

    Update

--- Laima.php
+++ Laima.php
@@ -3,11 +3,16 @@ Class Laima {
   public $name;
   public $price;
   public $color;
-  public function PriceCheck() {
+  public $weight;
+  public function AllPrice(10) {
+  $weight = $this->price / 10;
+  return $weight;
+}
+  /*public function PriceCheck() {
     If($this->price<=7) {
       return $answer = 'Дешёвые';
     } else {
       return $answer = 'Дорогие';
     }
-  }
+  }*/
 }
