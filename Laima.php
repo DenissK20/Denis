41f1e52ce71b9efd5d1cec27542d2d045033fb39
DenissK20 <deniska.riga@gmail.com>
2017-12-08 15:08:05 +0200

    g

---
+++ Laima.php
@@ -0,0 +1,17 @@
+<?php
+Class Laima {
+  public $name;
+  public $price;
+  public $color;
+  public function AllPrice($weight) {
+  $answer = $this->price * $weight;
+  return $answer;
+}
+  /*public function PriceCheck() {
+    If($this->price<=7) {
+      return $answer = 'Дешёвые';
+    } else {
+      return $answer = 'Дорогие';
+    }
+  }*/
+}
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require 'vendor/autoload.php';
-$app = new \atk4\ui\App('Добро пожаловать, геймер!');
+require 'Laima.php';
+$app = new \atk4\ui\App('Добро пожаловать на фабрику "Laima"!');
 $app->initLayout('Centered');
 /*$button = $app->add('Button');
 $button->set('My chess site');
@@ -53,8 +54,53 @@ $button2 = $app->layout->add(['Button', '2']);
 $button2->link(['text', 'number'=>'2']);
 
 $button3 = $app->layout->add(['Button', '3']);
-$button3->link(['text', 'number'=>'3']);*/
+$button3->link(['text', 'number'=>'3']);
 
 $text = $app->add(['Text', 'Hello noob! Click on the button!!! Faster!!!']);
 $button = $app->layout->add(['Button', 'Start!', 'large purple']);
 $button->link(['game', 'min'=>1, 'max'=>100]);
+
+$cat1 = new Cat;
+$cat1->name='Pups';
+$cat1->sex='Boy';
+$cat1->age='8';
+$cat1->color='Grey';
+$label1 = $app->add(['Label', $cat1->name.' '.($cat1->AgeCheck()),'image'=>'http://doskapozorakomi.ru/uploads/posts/2015-07/1437468703_image.jpg']);
+
+$cat2 = new Cat;
+$cat2->name='Sestra';
+$cat2->sex='Girl';
+$cat2->age='4';
+$cat2->color='Pink';
+$label2 = $app->add(['Label', $cat2->name.' '.($cat2->AgeCheck()), 'image'=>'http://club-pets.com/profile/obj/1039/board/1449/1449-4646b.jpg']);
+
+$cat3 = new Cat;
+$cat3->name='Attacker';
+$cat3->sex='Boy';
+$cat3->age='13';
+$cat3->color='Red';
+$label3 = $app->add(['Label', $cat3->name.' '.($cat3->AgeCheck()), 'image'=>'http://www.happy-bears.org/gallery/expos/Happy%20Bears%20Axel%20Golden%20Boy.%20Chisinau%2013-04-2013_669_ny11_62348.jpg']);*/
+
+$laima1 = new Laima;
+$laima1->name='Vaverīte';
+$laima1->price=7;
+$laima1->color='green';
+$label1 = $app->add(['Label', $laima1->name, $laima1->color.' massive', 'detail'=>$laima1->AllPrice(0.5), 'image'=>'http://www.laima.lv/wp-content/uploads/2016/05/4750001311662_V-VER-TE-2kg-konf-300x191.jpg']);
+
+$laima2 = new Laima;
+$laima2->name='Lukss';
+$laima2->price=9;
+$laima2->color='red';
+$label2 = $app->add(['Label', $laima2->name/*.' '.($laima2->PriceCheck())*/, $laima2->color.' massive', 'detail'=>$laima2->AllPrice(0.5), 'image'=>'https://nuko.lv/media/catalog/product/cache/1/image/480x740/9df78eab33525d08d6e5fb8d27136e95/0/0/00855201.jpg']);
+
+$laima3 = new Laima;
+$laima3->name='Lacīši';
+$laima3->price=5;
+$laima3->color='teal';
+$label3 = $app->add(['Label', $laima3->name/*.' '.($laima3->PriceCheck())*/, $laima3->color.' massive', 'detail'=>$laima3->AllPrice(0.5), 'image'=>'http://www.ekspobirojs.lv/Attachment/NomPicture/18224_1000x1000.jpg']);
+
+$laima4 = new Laima;
+$laima4->name='Serenāde';
+$laima4->price=8;
+$laima4->color='blue';
+$label4 = $app->add(['Label', $laima4->name/*.' '.($laima4->PriceCheck())*/, $laima4->color.' massive', 'detail'=>$laima4->AllPrice(0.5), 'image'=>'https://www.birojs.lv/global/uploads/images/products/8-02-003.bg.jpg']);
