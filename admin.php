7d55f389bc90d2d48b3b8c7231ef86bd70b9ef5a
DenissK20 <deniska.riga@gmail.com>
2018-02-23 16:02:19 +0200

    Update

--- admin.php
+++ admin.php
@@ -2,7 +2,16 @@
 session_start();
 require 'vendor/autoload.php';
 require 'connection.php';
+If((isset($_SESSION['hach'])) && ($_SESSION['hach']!='iqurhp23riuyfsfdugyozfixdbvkjn58946703jksdfg')) {
+  header('Location: error.php');
+
+}
+
 $app = new \atk4\ui\App('Админка');
 $app->initLayout('Centered');
+
 $crud=$app->layout->add('CRUD');
 $crud->setModel(new Friends($db));
+
+$button = $app->layout->add(['Button', 'Log out', 'purple']);
+$button->link(['logout']);
